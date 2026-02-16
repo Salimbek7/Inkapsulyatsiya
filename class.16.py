cdcb52da868454aa3238c5b30d7b00075d6ccc97
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2026-02-16 16:01:39 +0300

    Add files via upload

---
+++ class.16.py
@@ -0,0 +1,26 @@
+# class Salomlashish:
+#     def __init__(self,ism,fam):
+#         self.ism = ism
+#         self.__fam = fam
+#
+#     def get_fam(self):
+#         return self.__fam
+#
+#     # def salom(self):
+#     #     print(f"Assalomu aleykum hurmatli {self.ism} {self.fam} jamoamizga xush kelibsiz")
+#
+# salom1 = Salomlashish("Ali","Valiyev")
+# salom2 = Salomlashish("Ganijon","Norimov")
+# salom3 = Salomlashish("Bobur","Joliyev")
+#
+# print(salom1.get_fam())
+
+
+
+
+
+
+
+
+
+        
